435bbe062541ee9acb039ac9d3a4502753500b15
gabriel949 <gabriel.costa949@gmail.com>
2017-08-16 13:27:42 -0300

    first commit

---
+++ Trabalho/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Trabalho
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCadstrar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dgvCarro = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvVendidos = new System.Windows.Forms.DataGridView();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCadstrar
+            // 
+            this.btnCadstrar.BackColor = System.Drawing.Color.Gray;
+            this.btnCadstrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadstrar.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadstrar.ForeColor = System.Drawing.Color.White;
+            this.btnCadstrar.Location = new System.Drawing.Point(273, 84);
+            this.btnCadstrar.Name = "btnCadstrar";
+            this.btnCadstrar.Size = new System.Drawing.Size(220, 35);
+            this.btnCadstrar.TabIndex = 1;
+            this.btnCadstrar.Text = "Cadastrar";
+            this.btnCadstrar.UseVisualStyleBackColor = false;
+            this.btnCadstrar.Click += new System.EventHandler(this.btnCadstrar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.Gray;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExcluir.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(525, 84);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(220, 35);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Vender";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.BackColor = System.Drawing.Color.Gray;
+            this.btnProcurar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnProcurar.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcurar.ForeColor = System.Drawing.Color.White;
+            this.btnProcurar.Location = new System.Drawing.Point(273, 170);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(220, 36);
+            this.btnProcurar.TabIndex = 3;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = false;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(525, 170);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(220, 36);
+            this.txtNome.TabIndex = 4;
+            this.txtNome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dgvCarro
+            // 
+            this.dgvCarro.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCarro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarro.Location = new System.Drawing.Point(69, 260);
+            this.dgvCarro.Name = "dgvCarro";
+            this.dgvCarro.Size = new System.Drawing.Size(676, 177);
+            this.dgvCarro.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(763, 646);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 30);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Sair";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgvVendidos
+            // 
+            this.dgvVendidos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvVendidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVendidos.Location = new System.Drawing.Point(69, 493);
+            this.dgvVendidos.Name = "dgvVendidos";
+            this.dgvVendidos.Size = new System.Drawing.Size(676, 183);
+            this.dgvVendidos.TabIndex = 7;
+            this.dgvVendidos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVendidos_CellContentClick);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.Gray;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalvar.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(763, 84);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(92, 35);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar XML";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLer
+            // 
+            this.btnLer.BackColor = System.Drawing.Color.Gray;
+            this.btnLer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLer.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLer.ForeColor = System.Drawing.Color.White;
+            this.btnLer.Location = new System.Drawing.Point(763, 170);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(92, 35);
+            this.btnLer.TabIndex = 9;
+            this.btnLer.Text = "Ler XML";
+            this.btnLer.UseVisualStyleBackColor = false;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(337, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Carros Vendidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(337, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 22);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Carros Cadastrados";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(867, 687);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dgvVendidos);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dgvCarro);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCadstrar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCadstrar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DataGridView dgvCarro;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvVendidos;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Trabalho/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+//Gabriel Victor Oliveira de Paula Costa
+//Lucas Duarte Rocha
+//Felipe Bessa
+
+namespace Trabalho
+{
+    public partial class Inicio : MaterialForm
+    {
+        ArrayList Carros = new ArrayList();//Cria um Array List chamado Carros
+        ArrayList Vendidos = new ArrayList();//Cria um Array List chamado Vendidos
+        
+        public Inicio()//Incicializa o Form1
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        
+        }
+        public void CarregaGrid()//Função para carregar o Grid dos carros cadastrados,usa o array list Carros para alimentar o Grid
+        {
+            dgvCarro.DataSource = null;
+            dgvCarro.DataSource = Carros;
+
+            dgvCarro.ClearSelection();
+
+        }
+        public void CarregaGridVend()//Função para carregar o Grid dos carros vendidos,usa o array list Vendidos para alimentar o Grid
+        {
+            dgvVendidos.DataSource = null;
+            dgvVendidos.DataSource = Vendidos;
+
+            dgvVendidos.ClearSelection();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)//Evento que carrega o Grid de carros cadastrados e vendidos só quando houver 1 objeto ou mais.
+        {
+            if (Carros.Count != 0)
+            {
+                CarregaGrid();
+
+
+            }
+            if (Vendidos.Count != 0)
+            {
+
+                CarregaGridVend();
+
+            }
+        }
+        private void btnCadstrar_Click(object sender, EventArgs e)//Evento para quando clicar no botão "Cadastrar",
+                                                                  //cria as colinas no grid e adciona oque o usuário digitar no vetor Carros e carrega o novo grid
+        {
+            Carro x = new Carro();
+
+            x.Placa = "Digite a Placa ...";
+            x.Modelo = "";
+            x.Marca = "";
+            x.Ano = "";
+
+            Carros.Add(x);
+            CarregaGrid();
+            dgvCarro.CurrentCell = dgvCarro.Rows[dgvCarro.RowCount - 1].Cells[0];
+
+        }
+        private void dgvCarro_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            Carro x = new Carro();
+
+            x.Placa = dgvCarro.CurrentRow.Cells[0].Value.ToString();
+            x.Modelo = dgvCarro.CurrentRow.Cells[1].Value.ToString();
+            x.Marca = dgvCarro.CurrentRow.Cells[2].Value.ToString();
+            x.Ano = dgvCarro.CurrentRow.Cells[3].Value.ToString();
+
+            Carros[dgvCarro.CurrentRow.Index] = x;
+
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)//Quando clica no botão "Procurar" procura no Data Grid de carros cadastrados e
+                                                                  //no de vendidos para ver se há algum carro com a placa que o usuário digitou
+        {
+            int cont = 0;
+
+            string Nome = txtNome.Text;
+
+            txtNome.Text = "";
+
+            dgvCarro.ClearSelection();
+            dgvVendidos.ClearSelection();
+
+
+            for (int i = 0; i < dgvCarro.RowCount; i++)//Percorre o Data Grid de carros Cadastrados
+            {
+                if (dgvCarro.Rows[i].Cells[0].Value.ToString() == Nome)
+                {
+                    dgvCarro.CurrentCell = dgvCarro.Rows[i].Cells[0];
+
+                    cont++;
+                }
+
+            }
+
+            for (int i = 0; i < dgvVendidos.RowCount; i++)//Percorre o Data Grid de carros Vendidos
+            {
+                if (dgvVendidos.Rows[i].Cells[0].Value.ToString() == Nome)
+                {
+                    dgvVendidos.CurrentCell = dgvVendidos.Rows[i].Cells[0];
+
+                    cont++;
+                }
+
+            }
+
+
+            if (cont == 0)//Se não achar placa igual a digitada pelo usuário mostra este MessageBox
+            {
+                DialogResult Erro = new DialogResult();
+
+                string Texto = "Não existe carro com esta placa !!";
+                string Título = "Carro Inexistente!";
+                MessageBoxButtons BotãoMensagem = MessageBoxButtons.OK;
+                MessageBoxIcon ÍconeMensagem = MessageBoxIcon.Exclamation;
+
+                Erro = MessageBox.Show(Texto, Título, BotãoMensagem, ÍconeMensagem);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)//Ao clicar no botão sair,mostra uma Messagebox se o usuário deseja reamente sair
+                                                              // Se SIM,sai do programa,se NÃO,continua no programa
+        {
+            DialogResult Resp = new DialogResult();
+
+            string TextoMensagem = "Deseja SAIR do Programa?";
+            string TítuloMensagem = "Confirmação de Saída...";
+            MessageBoxButtons BotãoMensagem = MessageBoxButtons.YesNo;
+            MessageBoxIcon ÍconeMensagem = MessageBoxIcon.Question;
+
+            Resp = MessageBox.Show(TextoMensagem, TítuloMensagem, BotãoMensagem, ÍconeMensagem);        // Uma forma de uso...
+
+            if (Resp == DialogResult.Yes)         // Se o usuário confirmou...
+                Application.Exit();
+        }
+        private void btnExcluir_Click(object sender, EventArgs e)//Ao clicar no botão excluir apresenta messageBox perguntando se o usuário deseja vender o carro,
+                                                                 // e passa o objeto a ser vendido para o o array list Vendidos e é mostrado no data Grid de Vendidos 
+        {
+            DialogResult Excluir = new DialogResult();
+
+            string Texto = "Deseja realmente vender este carro ?";
+            string Título = "Vender ?";
+            MessageBoxButtons BotãoMensagem = MessageBoxButtons.YesNo;
+            MessageBoxIcon ÍconeMensagem = MessageBoxIcon.Exclamation;
+
+            Excluir = MessageBox.Show(Texto, Título, BotãoMensagem, ÍconeMensagem);
+            if (Excluir == DialogResult.Yes)
+            {
+
+                Carro x = new Carro();
+                try
+                {
+
+                    x.Placa = dgvCarro.CurrentRow.Cells[0].Value.ToString();
+                    x.Modelo = dgvCarro.CurrentRow.Cells[1].Value.ToString();
+                    x.Marca = dgvCarro.CurrentRow.Cells[2].Value.ToString();
+                    x.Ano = dgvCarro.CurrentRow.Cells[3].Value.ToString();
+
+                    Vendidos.Add(x);
+                    CarregaGridVend();
+                    Carros.RemoveAt(dgvCarro.CurrentRow.Index);
+                    CarregaGrid();
+                    CarregaGridVend();
+                }
+
+                catch (NullReferenceException Erro)
+                {
+
+                    string TextoExc = "Selecione algo para excluir!";
+                    MessageBoxButtons BotãoExc = MessageBoxButtons.OK;
+                    MessageBoxIcon ÍconeExc = MessageBoxIcon.Exclamation;
+
+                    MessageBox.Show(Erro.Message, TextoExc, BotãoExc, ÍconeExc);
+                }
+            }
+            
+        }
+
+        private void dgvVendidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)//Salva os dados do arrayList Carros e Vendidos em um arquivo XML
+        {
+            TextWriter Meuwriter = new StreamWriter(@"C:\POO\Carro.xml");
+            Carro[] CarroVetor = (Carro[])Carros.ToArray(typeof(Carro));
+            XmlSerializer serialização = new XmlSerializer(CarroVetor.GetType());
+            serialização.Serialize(Meuwriter, CarroVetor);
+            Meuwriter.Close();
+
+
+            TextWriter Meuwriter2 = new StreamWriter(@"C:\POO\Vendidos.xml");
+            Carro[] VendidosVetor = (Carro[])Vendidos.ToArray(typeof(Carro));
+            XmlSerializer serialização2 = new XmlSerializer(VendidosVetor.GetType());
+            serialização2.Serialize(Meuwriter2, VendidosVetor);
+            Meuwriter2.Close();
+
+            DialogResult salvo = new DialogResult();
+
+            string Texto = "XML Salvo com sucesso!";
+            string Título = "Salvo";
+            MessageBoxButtons BotãoMensagem = MessageBoxButtons.OK;
+            MessageBoxIcon ÍconeMensagem = MessageBoxIcon.Exclamation;
+
+            salvo = MessageBox.Show(Texto, Título, BotãoMensagem, ÍconeMensagem);
+
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)//Ao cicar no botão "Ler XML" lê os arquivos XML e escreve nos Data Grids as informações do XML
+        {
+            FileStream Arquivo = new FileStream(@"C:\POO\Carro.xml", FileMode.Open);
+            Carro[] VetorCarro = (Carro[])Carros.ToArray(typeof(Carro));
+            XmlSerializer Serialização = new XmlSerializer(VetorCarro.GetType());
+            VetorCarro = (Carro[])Serialização.Deserialize(Arquivo);
+            Carros.Clear();
+            Carros.AddRange(VetorCarro);
+            Arquivo.Close();
+            CarregaGrid();
+
+            FileStream Arquivo2 = new FileStream(@"C:\POO\Vendidos.xml", FileMode.Open);
+            Carro[] VendidosCarro = (Carro[])Vendidos.ToArray(typeof(Carro));
+            XmlSerializer Serialização2 = new XmlSerializer(VendidosCarro.GetType());
+            VendidosCarro = (Carro[])Serialização2.Deserialize(Arquivo2); 
+            Vendidos.Clear();
+            Vendidos.AddRange(VendidosCarro);
+            Arquivo2.Close();
+            CarregaGridVend();
+        }
+    }
+}
